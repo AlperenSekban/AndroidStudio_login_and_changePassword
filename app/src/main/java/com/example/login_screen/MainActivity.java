c0f85e8c4c7a5d0c97c5621fe58bae498c688746
AlperenSekban <alperensekban@outlook.com>
2020-07-04 02:36:31 +0300

    Login_Screen_And_ChangePassword_Screen

--- app/src/main/java/com/example/login_screen/MainActivity.java
+++ app/src/main/java/com/example/login_screen/MainActivity.java
@@ -2,7 +2,9 @@ package com.example.login_screen;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -10,47 +12,53 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    SharedPreferences sharedPreferences;
+    SharedPreferences.Editor editor;
+
     Button loginBtn;
-    TextView userNameTxt,passwordTxt;
-    String userName="admin";
-    String password="User123!";
+    TextView userNameTxt, passwordTxt;
+    String userName;
+    String password;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        loginBtn =(Button) findViewById(R.id.loginBtn);
-        userNameTxt =(TextView) findViewById(R.id.userNameTxt);
-        passwordTxt=(TextView) findViewById(R.id.passwordTxt);
+        loginBtn = findViewById(R.id.activity_main_login_button);
+        userNameTxt = findViewById(R.id.activity_main_username_textInputLayout);
+        passwordTxt = findViewById(R.id.activitiy_main_password_textInputLayout);
+        sharedPreferences = getApplicationContext().getSharedPreferences("saklas", MODE_PRIVATE);
+        editor = sharedPreferences.edit();
+        userName = "admin";
+        password = sharedPreferences.getString(Utility.password, "User123!");
 
-        Bundle bundle= getIntent().getExtras();
-        if(bundle!=null){
-            password=bundle.getString("password");
-            ;
+
+        Bundle bundle = getIntent().getExtras();
+        if (bundle != null) {
+            editor.putString(Utility.password, bundle.getString(Utility.password, "User123!"));
+            editor.commit();
+
+
         }
 
         loginBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                if(userNameTxt.getText().toString().equals(userName)&& passwordTxt.getText().toString().equals(password)){
-                    Intent intent=new Intent(MainActivity.this,PasswordChangeScreen.class);
+                if (userNameTxt.getText().toString().equals(userName) && passwordTxt.getText().toString().equals(password)) {
+                    Intent intent = new Intent(MainActivity.this, PasswordChangeScreen.class);
                     startActivity(intent);
+                    finish();
 
-                }
-                else{
-                    Toast.makeText(getApplicationContext(),"Kullanıcı adı veya şifrenizi yanlış girdiniz...",Toast.LENGTH_SHORT).show();
+                } else {
+                    Toast.makeText(getApplicationContext(), R.string.activity_main_toastMessage_wrongLogIn, Toast.LENGTH_SHORT).show();
                 }
 
             }
         });
 
 
-
     }
-
-
-
 
 
 }
--- app/src/main/java/com/example/login_screen/PasswordChangeScreen.java
+++ app/src/main/java/com/example/login_screen/PasswordChangeScreen.java
@@ -20,25 +20,29 @@ public class PasswordChangeScreen extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_password_change_screen);
 
-        passwordChangeBtn=(Button) findViewById(R.id.passwordChangeBtn);
-        passwordChangeTxt=(TextView) findViewById(R.id.passwordChangeTxt);
+        passwordChangeBtn = findViewById(R.id.activity_password_change_screen_passwordChange_button);
+        passwordChangeTxt = findViewById(R.id.activity_password_change_screen_passwordChange_textInputLayout);
 
         passwordChangeBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                if(passwordChangeTxt.getText().toString().length()<6){
-                    Toast.makeText(getApplicationContext(),"Şifreniz minimum 6 karakter içermek zorundadır",Toast.LENGTH_SHORT).show();
-                }
-                if(!(IsTherePunctuation(passwordChangeTxt.getText().toString()))){
-                    Toast.makeText(getApplicationContext(),"Şifreniz noktalama işareti içermek zorundadır",Toast.LENGTH_SHORT).show();
-                }
-                if(!(IsThereUpperLatter(passwordChangeTxt.getText().toString()))){
-                    Toast.makeText(getApplicationContext(),"Şifreniz büyük harf içermek zorundadır",Toast.LENGTH_SHORT).show();
-                }
-                else{
-                    Intent intent=new Intent(PasswordChangeScreen.this,MainActivity.class);
-                    intent.putExtra("password",passwordChangeTxt.getText().toString());
-                    startActivity(intent);
+                if (passwordChangeTxt.getText().toString().isEmpty()) {
+                    Toast.makeText(getApplicationContext(), R.string.activity_password_change_screen_toastMessage_null, Toast.LENGTH_SHORT).show();
+                } else {
+                    if (passwordChangeTxt.getText().toString().length() < 6) {
+                        Toast.makeText(getApplicationContext(), R.string.activity_password_change_screen_toastMessage_minimumSixCharacter, Toast.LENGTH_SHORT).show();
+                    }
+                    if (!(IsTherePunctuation(passwordChangeTxt.getText().toString()))) {
+                        Toast.makeText(getApplicationContext(), R.string.activity_password_change_screen_toastMessage_punctuation, Toast.LENGTH_SHORT).show();
+                    }
+                    if (!(IsThereUpperLatter(passwordChangeTxt.getText().toString()))) {
+                        Toast.makeText(getApplicationContext(), R.string.activity_password_change_screen_toastMessage_upperLatter, Toast.LENGTH_SHORT).show();
+                    }
+                    if (passwordChangeTxt.getText().toString().length() >= 6 && (IsTherePunctuation(passwordChangeTxt.getText().toString())) && (IsThereUpperLatter(passwordChangeTxt.getText().toString()))) {
+                        Intent intent = new Intent(PasswordChangeScreen.this, MainActivity.class);
+                        intent.putExtra(Utility.password, passwordChangeTxt.getText().toString());
+                        startActivity(intent);
+                    }
                 }
             }
 
@@ -46,28 +50,28 @@ public class PasswordChangeScreen extends AppCompatActivity {
         });
 
 
-
     }
-    public boolean IsTherePunctuation(String password){
-        int flag=0;
-        for(int i=0;i<password.length();i++){
-            if ((password.charAt(i)>32 && password.charAt(i)<48) || (password.charAt(i)>57 && password.charAt(i)<65) || (password.charAt(i)>90 && password.charAt(i)<97) || (password.charAt(i)>122 && password.charAt(i)<127)){
+
+    public boolean IsTherePunctuation(String password) {
+        int flag = 0;
+        for (int i = 0; i < password.length(); i++) {
+            if ((password.charAt(i) > 32 && password.charAt(i) < 48) || (password.charAt(i) > 57 && password.charAt(i) < 65) || (password.charAt(i) > 90 && password.charAt(i) < 97) || (password.charAt(i) > 122 && password.charAt(i) < 127)) {
                 flag++;
 
             }
         }
-        return (flag>0);
+        return (flag > 0);
     }
 
-    public boolean IsThereUpperLatter(String password){
-        int flag=0;
-        for(int i=0;i<password.length();i++){
-            if ((password.charAt(i)>64 && password.charAt(i)<91)){
+    public boolean IsThereUpperLatter(String password) {
+        int flag = 0;
+        for (int i = 0; i < password.length(); i++) {
+            if ((password.charAt(i) > 64 && password.charAt(i) < 91)) {
                 flag++;
 
             }
         }
-        return (flag>0);
+        return (flag > 0);
 
     }
 }
